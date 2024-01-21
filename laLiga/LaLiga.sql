8771bd188927e6bd8cc4d213f6541c171c7a01d2
BladNunez <Nunez.Bladimir@yahoo.com>
2024-01-21 15:44:19 -0500

    new sql file

--- laLiga/LaLiga.sql
+++ laLiga/LaLiga.sql
@@ -1,10 +1,22 @@
 use LaLiga;
 
+alter table liga modify column Goals int;
+alter table liga modify column Age int;
+alter table liga modify column Assist int;
+alter table liga modify column YC int;
+alter table liga modify column RC int;
+alter table liga modify column SPG float;
+alter table liga modify column `PS%` float;
+alter table liga modify column AW float;
+alter table liga modify column MOTM int;
+alter table liga modify column Rating float;
+alter table liga modify column MinP int;
+
 # 1. What is the average age of players in the dataset?
 select round(avg(Age)) as AVG_AGE from liga;
 
 # 2. Which player scored the highest number of goals?
-select Player_Name, Goals as most_goals from liga where Goals = (select max(Goals) from liga);
+select Player_Name, Goals from liga order by Goals Desc limit 1;
 
 # 3. Which player has the highest number of assists?
 select Player_Name, Assist as most_assists from liga where Assist = (select max(Assist) from liga);
@@ -26,3 +38,7 @@ select round(avg(AW),2) as avg_aerial_duel_wins from liga;
 
 # 9. 'Man of the Match' (MOTM) top 5 most awarded players?
 select Player_Name, max(MOTM) from liga group by Player_Name order by max(MOTM) desc limit 5;
+
+# 10. Top 5 players who played the most minutes
+select Team_Name, Player_Name, MinP from liga order by MinP desc limit 5;
+
